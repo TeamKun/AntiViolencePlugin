e66f2d5e972d737bb674a5dc0ab8a627633c18c8
Yu212 <41502287+Yu212@users.noreply.github.com>
2021-08-22 19:55:18 +0900

    ダメージを与える方式を変更、雪玉に対応

--- src/main/java/net/kunmc/lab/antiviolence/AttackEventListener.java
+++ src/main/java/net/kunmc/lab/antiviolence/AttackEventListener.java
@@ -1,30 +1,19 @@
 package net.kunmc.lab.antiviolence;
 
 import net.kunmc.lab.antiviolence.config.ConfigManager;
-import net.minecraft.server.v1_16_R3.*;
-import org.bukkit.Bukkit;
+import net.minecraft.server.v1_16_R3.DamageSource;
+import net.minecraft.server.v1_16_R3.EnchantmentManager;
+import net.minecraft.server.v1_16_R3.EntityHuman;
+import net.minecraft.server.v1_16_R3.MathHelper;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftArrow;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftLivingEntity;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_16_R3.util.CraftVector;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityExhaustionEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerVelocityEvent;
-import org.bukkit.util.Vector;
-
-import java.util.Iterator;
-import java.util.List;
 
 public class AttackEventListener implements Listener {
-    @EventHandler
-    public void onAttack(PlayerMoveEvent event) {
-    }
-
     @EventHandler
     public void onAttack(EntityDamageByEntityEvent event) {
         ConfigManager configManager = AntiViolencePlugin.getInstance().getConfigManager();
@@ -34,33 +23,63 @@ public class AttackEventListener implements Listener {
         if (event.getEntity().getUniqueId().equals(event.getDamager().getUniqueId())) {
             return;
         }
-        if (!(event.getEntity() instanceof LivingEntity)) {
+        if (!(event.getEntity() instanceof CraftLivingEntity)) {
             return;
         }
+        CraftLivingEntity revenger = (CraftLivingEntity)event.getEntity();
         double damageMultiplier = configManager.getDamageMultiplier();
-        double returnedAmount = event.getDamage() * damageMultiplier;
+        double returnedAmount = event.getFinalDamage() * damageMultiplier;
         boolean immediatelyKill = configManager.isImmediatelyKill();
         if (event.getDamager() instanceof CraftPlayer) {
-            revenge((CraftPlayer)event.getDamager(), returnedAmount, immediatelyKill);
+            CraftPlayer damager = (CraftPlayer)event.getDamager();
+            if (immediatelyKill) {
+                damager.setHealth(0);
+            } else {
+                revenge(damager, revenger, (float)returnedAmount);
+            }
         } else if (event.getDamager() instanceof Projectile) {
             Projectile damagerProjectile = (Projectile)event.getDamager();
             if (damagerProjectile.getShooter() instanceof CraftPlayer) {
-                revenge((CraftPlayer)damagerProjectile.getShooter(), returnedAmount, immediatelyKill);
+                CraftPlayer damager = (CraftPlayer)damagerProjectile.getShooter();
+                if (immediatelyKill) {
+                    damager.setHealth(0);
+                } else {
+                    revengeByProjectile(damager, damagerProjectile, revenger, (float)returnedAmount);
+                }
             }
         }
     }
 
-    private void revenge(CraftPlayer damager, double returnedAmount, boolean absolutelyKill) {
-        if (absolutelyKill) {
-            damager.setHealth(0);
-            return;
+    private void revenge(CraftPlayer damager, CraftLivingEntity revenger, float returnedAmount) {
+        DamageSource damageSource;
+        if (revenger.getHandle() instanceof EntityHuman) {
+            damageSource = DamageSource.playerAttack((EntityHuman)revenger.getHandle());
+        } else {
+            damageSource = DamageSource.mobAttack(revenger.getHandle());
         }
-        damager.damage(returnedAmount);
-        float yaw = damager.getEyeLocation().getYaw();
+        damager.getHandle().damageEntity(damageSource, returnedAmount);
         int knockback = EnchantmentManager.b(damager.getHandle());
         if (damager.isSprinting() && damager.getAttackCooldown() > 0.9F) {
             knockback++;
         }
+        float yaw = damager.getEyeLocation().getYaw();
         damager.getHandle().a(knockback * 0.5F, -MathHelper.sin(yaw * 0.017453292F), MathHelper.cos(yaw * 0.017453292F));
+    }
+
+    private void revengeByProjectile(CraftPlayer damager, Projectile damagerProjectile, CraftLivingEntity revenger, float returnedAmount) {
+        returnedAmount += 1e-9;
+        DamageSource damageSource;
+        if (revenger.getHandle() instanceof EntityHuman) {
+            damageSource = DamageSource.playerAttack((EntityHuman)revenger.getHandle());
+        } else {
+            damageSource = DamageSource.mobAttack(revenger.getHandle());
+        }
+        damager.getHandle().damageEntity(damageSource, returnedAmount);
+        float yaw = damager.getEyeLocation().getYaw();
+        int knockbackStrength = 0;
+        if (damagerProjectile instanceof CraftArrow) {
+            knockbackStrength = ((CraftArrow)damagerProjectile).getKnockbackStrength();
+        }
+        damager.getHandle().a(knockbackStrength * 0.6F, -MathHelper.sin(yaw * 0.017453292F), MathHelper.cos(yaw * 0.017453292F));
     }
 }
